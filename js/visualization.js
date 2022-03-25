793b6a21ff0c903647d9462cbad5119f0305a101
Floris Dobber <florisdobber@gmail.com>
2022-03-25 10:28:48 -0400

    Converted files to CSVs and logged data

--- js/visualization.js
+++ js/visualization.js
@@ -4,3 +4,13 @@ d3.select("#vis-container")
   .attr("x", 20)
   .attr("y", 20)
   .text("Hello World!");
+
+d3.csv("data/player_data.csv").then((data) => {
+  // Log the first 10 rows of player data to the console
+  console.log(data.slice(0, 10));
+});
+
+d3.csv("data/team_data.csv").then((data) => {
+  // Log the first 10 rows of team data to the console
+  console.log(data.slice(0, 10));
+});
